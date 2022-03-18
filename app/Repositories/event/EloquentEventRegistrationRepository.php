27eb7289bf28e9fffc8a062038067f77e2af9b39
Lucy <urandelger108@gmail.com>
2022-03-18 17:18:50 +0800

    repo

--- app/Repositories/event/EloquentEventRegistrationRepository.php
+++ app/Repositories/event/EloquentEventRegistrationRepository.php
@@ -114,10 +114,9 @@ class EloquentEventRegistrationRepository implements EventRegistrationRepository
 				$actionHtml .= '<ul class="nav nav-hoverable flex-column">';
 				if($qry->status == @Config::get('smart.event_registeation_status')['created'])
 				{
-					
+					$actionHtml .= 	'<li class="nav-item"><a class="nav-link edit" href="javascript:;" data-registrationid="'.$qry->id.'"><i class="nav-icon flaticon-edit-1"></i><span class="nav-text">'.trans('display.general_edit').'</span></a></li>';
+					$actionHtml .= 	'<li class="nav-item"><a class="nav-link delete" href="javascript:;" data-registrationid="'.$qry->id.'"><i class="nav-icon flaticon-delete"></i><span class="nav-text">'.trans('display.general_delete').'</span></a></li>';
 				}
-				$actionHtml .= 	'<li class="nav-item"><a class="nav-link edit" href="javascript:;" data-registrationid="'.$$qry->id.'"><i class="nav-icon flaticon-edit-1"></i><span class="nav-text">'.trans('display.general_edit').'</span></a></li>';
-				$actionHtml .= 	'<li class="nav-item"><a class="nav-link delete" href="javascript:;" data-registrationid="'.$$qry->id.'"><i class="nav-icon flaticon-delete"></i><span class="nav-text">'.trans('display.general_delete').'</span></a></li>';
 				$actionHtml .= '</ul>';
 				$actionHtml .= '</div>';
 				$actionHtml .= '</div>';
