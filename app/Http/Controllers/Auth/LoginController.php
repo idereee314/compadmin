63b866c5ed7376de2028ac581e1b35bda60ac564
Lucy <urandelger108@gmail.com>
2022-03-20 19:51:47 +0800

    com

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -64,7 +64,7 @@ class LoginController extends Controller
 
 			Session::flash('message', trans('messages.error_login'));
 
-			return Redirect::to('/login')
+			return Redirect::to('/')
 				->withErrors($validator)
 				->withInput(Request::except('password'));
 		} else {
@@ -87,7 +87,7 @@ class LoginController extends Controller
 
 			} else {
 				Session::flash('message', trans('messages.error_login'));
-				return Redirect::to('/login');
+				return Redirect::to('/');
 			}
 		}
     }
@@ -102,7 +102,7 @@ class LoginController extends Controller
 		Auth::logout();
 		Session::flush();
 
-    	return Redirect::to('/login');
+    	return Redirect::to('/');
 	}
 
 	public function showLogin() {
