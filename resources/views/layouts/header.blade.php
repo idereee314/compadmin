0d84dc10327aef5055db75227685abc831cb2046
NEba <frozen.at34@gmail.com>
2022-03-21 11:15:34 +0800

    fix button

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -4,7 +4,7 @@
         <!--begin::Left-->
         <div class="d-none d-lg-flex align-items-center mr-3">
             <!--begin::Aside Toggle-->
-            {{-- <button class="btn btn-icon aside-toggle ml-n3 mr-10" id="kt_aside_desktop_toggle">
+            <button class="btn btn-icon aside-toggle ml-n3 mr-10" id="kt_aside_desktop_toggle">
                 <span class="svg-icon svg-icon-xxl svg-icon-dark-75">
                     <!--begin::Svg Icon | path:assets/media/svg/icons/Text/Align-left.svg-->
                     <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
@@ -17,7 +17,7 @@
                     </svg>
                     <!--end::Svg Icon-->
                 </span>
-            </button> --}}
+            </button>
             <!--end::Aside Toggle-->
             <!--begin::Logo-->
             <a href="/home">
