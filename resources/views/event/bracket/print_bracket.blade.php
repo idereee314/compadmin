a53a62eccfb85e6381f7faaf488aab5a59a4bf78
Xvvkv <kenji.batka@gmail.com>
2022-05-20 11:10:12 +0800

    bracket

--- resources/views/event/bracket/print_bracket.blade.php
+++ resources/views/event/bracket/print_bracket.blade.php
@@ -17,10 +17,10 @@
             </td>
         </tr>
     </table>
-    <table width="100%" style="width:100%" id="table1" border="1">
+    <table width="100%" style="width:100%;" id="table1" border="0" class="lvlone-table">
         <tr>
             @for($i = 0; $i < $round; $i++)
-            <th width="{{100/$round}}%">Тойрог {{ $i + 1 }}</th>
+            <th style="padding-bottom: 10px;" width="{{100/$round}}%">Тойрог {{ $i + 1 }}</th>
             @endfor
         </tr>
         <?php $k = 0; ?>
@@ -29,23 +29,40 @@
             @for($i = 0; $i < $round; $i++)
                 @if($i == 0)
                     <td>    
-                        <table width="100%" style="width:100%" id="table1">
-                            <tr>
-                                <td width="50%" align="center" style="font-size: 11px;border-right: 1px solid #cdd0d4;">
-                                {!! $member->lastname_one != null? $member->lastname_one.' <strong>'.$member->firstname_one.'</strong>': 'BYE'!!}<br>
-                                {{$member->acname_one}}
-                                </td>
-                                <td width="50%" align="center" style="font-size: 11px;">
-                                {!! $member->lastname_two != null? $member->lastname_two.' <strong>'.$member->firstname_two.'</strong>': 'BYE'!!}<br>
-                                {{$member->acname_two}}
-                                </td>
-                            </tr>
-                        </table>
+                        <div class="connector">
+                            <div class="linebox"></div>
+                            <div class="linebox_two"></div>
+                            <table width="100%" style="width:100%;" id="table1" border="1">
+                                <tr>
+                                    <td width="50%" align="center" style="font-size: 11px;border-right: 1px solid #cdd0d4;">
+                                    {!! $member->lastname_one != null? $member->lastname_one.' <strong>'.$member->firstname_one.'</strong>': 'BYE'!!}<br>
+                                    {{$member->acname_one}}
+                                    </td>
+                                    <td width="50%" align="center" style="font-size: 11px;">
+                                    {!! $member->lastname_two != null? $member->lastname_two.' <strong>'.$member->firstname_two.'</strong>': 'BYE'!!}<br>
+                                    {{$member->acname_two}}
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
                     </td>
                 @else
                     @if($k % pow(2, $i) == 0)
                     <td rowspan="{{ pow(2, $i) }}">
-                            
+                        <div class="connector">
+                            <div class="linebox"></div>
+                            <div class="linebox_two"></div>
+                            <table width="100%" style="width:100%;" id="table1" border="1">
+                                <tr>
+                                    <td width="50%" align="center" style="font-size: 11px;border-right: 1px solid #cdd0d4;">
+                                    TBD
+                                    </td>
+                                    <td width="50%" align="center" style="font-size: 11px;">
+                                    TBD
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
                     </td>
                     @endif
                 @endif
@@ -62,3 +79,51 @@
     </table>
     
 </div>
+
+<style>
+    .lvlone-table tr td{
+        padding-bottom: 5px;
+        height: inherit;
+    }
+    .lvlone-table tr td table{
+        min-height: 50px;
+    }
+    .lvlone-table tr td:first-child .connector{
+        border: none;
+        padding-left: 0px;
+    }
+    .lvlone-table tr td:first-child .linebox{
+        display: none;
+    }
+    .lvlone-table tr td:first-child .linebox_two{
+        display: none;
+    }    
+    .lvlone-table tr{
+        height: 1px;
+    }
+    .lvlone-table tr td{
+        height: inherit;
+    }
+    .connector{
+        display: flex;
+        align-items: center;
+        position: relative;
+        padding-left: 15px;
+        height: 50%;
+    }
+    .linebox{
+        height: 100%;
+        width: 8px;
+        position: absolute;
+        border: 2px solid #444;
+        border-left-width: 0px;
+        margin-left: -17px;
+    }
+    .linebox_two{
+        height: 2px;
+        width: 8px;
+        position: absolute;
+        background: #444;
+        margin-left: -7px;
+    }
+</style>
