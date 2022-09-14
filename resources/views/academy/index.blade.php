d3b9b44f8944a8a3f3c55811b5fc536784154cb6
NEba <frozen.at34@gmail.com>
2022-09-14 11:02:40 +0800

    academy name search

--- resources/views/academy/index.blade.php
+++ resources/views/academy/index.blade.php
@@ -31,6 +31,56 @@
                                 </div>
                             </div>
                             <div class="card-body">
+
+                                <div class="accordion accordion-light accordion-light-borderless accordion-svg-toggle" id="search">
+                                    <div class="card">
+                                        <div class="card-header">
+                                            <div class="card-title collapsed" data-toggle="collapse" data-target="#search-academy">
+                                                <span class="svg-icon svg-icon-primary">
+                                                    <!--begin::Svg Icon | path:assets/media/svg/icons/Navigation/Angle-double-right.svg-->
+                                                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                                                        <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                                            <polygon points="0 0 24 0 24 24 0 24" />
+                                                            <path d="M12.2928955,6.70710318 C11.9023712,6.31657888 11.9023712,5.68341391 12.2928955,5.29288961 C12.6834198,4.90236532 13.3165848,4.90236532 13.7071091,5.29288961 L19.7071091,11.2928896 C20.085688,11.6714686 20.0989336,12.281055 19.7371564,12.675721 L14.2371564,18.675721 C13.863964,19.08284 13.2313966,19.1103429 12.8242777,18.7371505 C12.4171587,18.3639581 12.3896557,17.7313908 12.7628481,17.3242718 L17.6158645,12.0300721 L12.2928955,6.70710318 Z" fill="#000000" fill-rule="nonzero" />
+                                                            <path d="M3.70710678,15.7071068 C3.31658249,16.0976311 2.68341751,16.0976311 2.29289322,15.7071068 C1.90236893,15.3165825 1.90236893,14.6834175 2.29289322,14.2928932 L8.29289322,8.29289322 C8.67147216,7.91431428 9.28105859,7.90106866 9.67572463,8.26284586 L15.6757246,13.7628459 C16.0828436,14.1360383 16.1103465,14.7686056 15.7371541,15.1757246 C15.3639617,15.5828436 14.7313944,15.6103465 14.3242754,15.2371541 L9.03007575,10.3841378 L3.70710678,15.7071068 Z" fill="#000000" fill-rule="nonzero" opacity="0.3" transform="translate(9.000003, 11.999999) rotate(-270.000000) translate(-9.000003, -11.999999)" />
+                                                        </g>
+                                                    </svg>
+                                                    <!--end::Svg Icon-->
+                                                </span>
+                                                <div class="card-label pl-4">Хайлт</div>
+                                            </div>
+                                        </div>
+                                        <div id="search-academy" class="collapse" data-parent="#search">
+                                            <div class="card-body">
+                                                <!--begin: Search Form-->
+                                                <form class="mb-10" id="academy-search-form" method="POST">
+                                                    <div class="row mb-6">
+                                                        <div class="col-lg-2 mb-lg-0 mb-6">
+                                                            <label>{{trans('display.comp_academy_name')}}</label>
+                                                            <input type="text" class="form-control datatable-input" name="name" id="name" data-col-index="1">
+                                                        </div>
+                                                    </div>
+                                                    <div class="row mt-8">
+                                                        <div class="col-lg-12">
+                                                            <button type="submit" class="btn btn-primary btn-primary--icon">
+                                                                <span>
+                                                                    <i class="la la-search"></i>
+                                                                    <span>{{ trans('display.general_search') }}</span>
+                                                                </span>
+                                                            </button>
+                                                            <button type="reset" class="btn btn-secondary btn-secondary--icon" id="kt_reset">
+                                                                <span>
+                                                                    <i class="la la-close"></i>
+                                                                    <span>{{ trans('display.general_reset') }}</span>
+                                                                </span>
+                                                            </button>
+                                                        </div>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                                 <!--begin: Datatable-->
                                 <div id="kt_datatable_wrapper" class="dataTables_wrapper dt-bootstrap4 no-footer">
                                     <div class="panel-sub-heading">
@@ -90,9 +140,7 @@ $(document).ready(function() {
             url: '{{route('academy.data.list')}}',
             type: 'POST',
             data: function ( d ) {
-                d.name = $('#user-search-form input[id="name"]').val();
-                d.email = $('#user-search-form input[id="email"]').val();
-                d.role = $('#user-search-form select[id="role"]').val();
+                d.name = $('#academy-search-form input[id="name"]').val();
             },
         },
         columns: [
@@ -132,7 +180,7 @@ $(document).ready(function() {
         }]
 	});
 
-    $('#user-search-form').on('submit', function(e) {
+    $('#academy-search-form').on('submit', function(e) {
         academyTable.draw();
         e.preventDefault();
     });
