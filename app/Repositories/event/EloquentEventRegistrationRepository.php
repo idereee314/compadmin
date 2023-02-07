e46d6c40b0536ae66836e4945e75072da851789f
Erdenebayar Erdenebileg <erdenebayar.erdenebileg@gmail.com>
2023-02-07 13:48:23 +0800

    Changes

--- app/Repositories/event/EloquentEventRegistrationRepository.php
+++ app/Repositories/event/EloquentEventRegistrationRepository.php
@@ -404,11 +404,11 @@ class EloquentEventRegistrationRepository implements EventRegistrationRepository
 
 	public function getBracketMembersFromEvent($eventId, $entryId, $entryAgeId, $entryBeltId, $entryWeightId, $isWeightChecked = false)
 	{
+		//and is_weight_checked = false
 		return DB::select("select r.id, r.member_id, r.academy_id, case when a.is_other = 1 then r.academy_name else a.name end as acname 
 								from uq_comp.uq_event_registration r
 								inner join uq_comp.uq_academy a on r.academy_id = a.id 
-								where status = 'approved' and event_id = ".$eventId." 
-								and is_weight_checked = false
+								where status = 'approved' and event_id = ".$eventId." 								
 								and r.entry_id = ".$entryId." and r.entry_age_id = ".$entryAgeId." 
 								and r.entry_belt_id = ".$entryBeltId." and r.entry_weight_id = ".$entryWeightId."
 								order by r.academy_id, r.academy_name, r.id");
