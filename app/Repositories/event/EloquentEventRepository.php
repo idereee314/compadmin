0611678b6825c6b8b66669326ef633a6beb20e79
idereee314 <idereeee.engineering@gmail.com>
2023-04-10 18:30:35 +0800

    fixed competition card view

--- app/Repositories/event/EloquentEventRepository.php
+++ app/Repositories/event/EloquentEventRepository.php
@@ -52,17 +52,34 @@ class EloquentEventRepository implements EventRepository {
 	public function getEventByPage($perPage = 10, $searchData = null)
 	{
 
-		$qry = Event::selectRaw('rti_event.id, rti_event.name, rti_event.description, rti_event.event_date, rti_event.due_date, uq_event_config.reg_start_date, uq_event_config.reg_end_date')
-			->join('uq_event_config', 'uq_event_config.event_id', '=', 'rti_event.id')
-			->with(['picturesMobileCover:event_id,dir_url,url', 'members:id,profile_url,firstname,lastname,gender_code'])
-			->withCount(['registration', 'registration as status_approved' => function ($q) {
-				$q->where('uq_event_registration.status', @Config::get('smart.event_registration_status')['approved']);
-			}])
-			->join('uq_event_user', 'uq_event_user.event_id', '=', 'rti_event.id')
-			->where('uq_event_user.user_id', '=', Auth::user()->id)
-			->orderBy('rti_event.event_date', 'desc');
+		$isAdmin = DB::table('uniqdb.uq_comp.uq_compad_user_role')
+            ->join('uniqdb.uq_comp.uq_compad_role', 'uniqdb.uq_comp.uq_compad_role.id', '=', 'uniqdb.uq_comp.uq_compad_user_role.role_id')
+            ->where('uniqdb.uq_comp.uq_compad_user_role.user_id', '=', Auth::user()->id)
+            ->where('uniqdb.uq_comp.uq_compad_role.code', '=', 'admin')
+            ->exists();
 
-		$eventConfig = $qry->orderBy('uq_event_config.created_at', 'desc')->paginate($perPage);
-		return json_encode($eventConfig);
+		if ($isAdmin) {
+		    $qry = Event::selectRaw('rti_event.id, rti_event.name, rti_event.description, rti_event.event_date, rti_event.due_date, uq_event_config.reg_start_date, uq_event_config.reg_end_date')
+		        ->join('uq_event_config', 'uq_event_config.event_id', '=', 'rti_event.id')
+		        ->with(['picturesMobileCover:event_id,dir_url,url', 'members:id,profile_url,firstname,lastname,gender_code'])
+		        ->withCount(['registration', 'registration as status_approved' => function ($q) {
+		            $q->where('uq_event_registration.status', @Config::get('smart.event_registration_status')['approved']);
+		        }]);
+		} else {
+		    $qry = Event::selectRaw('rti_event.id, rti_event.name, rti_event.description, rti_event.event_date, rti_event.due_date, uq_event_config.reg_start_date, uq_event_config.reg_end_date')
+		        ->join('uq_event_config', 'uq_event_config.event_id', '=', 'rti_event.id')
+		        ->with(['picturesMobileCover:event_id,dir_url,url', 'members:id,profile_url,firstname,lastname,gender_code'])
+		        ->withCount(['registration', 'registration as status_approved' => function ($q) {
+		            $q->where('uq_event_registration.status', @Config::get('smart.event_registration_status')['approved']);
+		        }])
+		        ->join('uq_event_user', 'uq_event_user.event_id', '=', 'rti_event.id')
+		        ->where('uq_event_user.user_id', '=', Auth::user()->id)
+		        ->orderBy('rti_event.event_date', 'desc');
+		}
+
+
+	    $eventConfig = $qry->orderBy('uq_event_config.created_at', 'desc')->paginate($perPage);
+	    return json_encode($eventConfig);
 	}
+
 }
