d8eb4e9ef698751a4a5f27cba8ea1f70c907554e
idereee314 <idereee.engineering@gmail.com>
2026-03-21 10:56:38 +0800

    changess

--- app/Repositories/event/matches/RoundRobinStrategy.php
+++ app/Repositories/event/matches/RoundRobinStrategy.php
@@ -178,8 +178,8 @@ class RoundRobinStrategy implements TournamentEliminationStrategy {
      */
     private function updateStandings($bracketId, $winnerId, $participantOneId, $participantTwoId)
     {
-        $winPoints = 3;
-        $lossPoints = 0;
+        $winPoints = $matchData['win_points'] ?? 3;
+        $lossPoints = $matchData['loss_points'] ?? 0;
 
         Log::info('Standings updated', [
             'bracket_id' => $bracketId,
